b2572ad5384276754e8ca61c48c55acece60b8e0
adrimihailescu <88376680+adrimihailescu@users.noreply.github.com>
2021-09-04 21:03:10 +0100

    added search functionality; refactored folder structure

---
+++ src/containers/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import { robots } from '../robots';
+import Scroll from '../components/Scroll';
+import './App.css'
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+
+
+
+    }
+    render() {
+        const filtereredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className='tc' >
+                <h1 className='f1' >RoboFriends</h1>
+                <SearchBox onSearchChange={this.onSearchChange} />
+                <Scroll>
+                    <CardList robots={filtereredRobots} />
+                </Scroll>
+            </div >
+        );
+    }
+}
+export default App;
